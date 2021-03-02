a4967e356f2404f5bba65213ce91b211186e1496
siznax <steve@siznax.net>
2021-03-01 17:00:31 -0800

    Remove unused keypress actions and do not override [space]

--- Moo/play/static/js/keyboard.js
+++ Moo/play/static/js/keyboard.js
@@ -2,31 +2,11 @@ function keyPressed(e) {
 
     // alert(e.code)
 
-    if (e.code == "Space") {
-        e.preventDefault()
-        if (audio.paused) { 
-            audio.play() 
-            // glowOn()
-        }
-        else { 
-            audio.pause() 
-            // glowOff()
-        }
-    } 
+    if (e.code == "KeyN") { gotoNext() }
 
-    else if (e.code == "KeyN") { gotoNext() }
     else if (e.code == "KeyP") { gotoPrev() }
     else if (e.code == "KeyR") { gotoRandom() }
     else if (e.code == "KeyT") { playRandomTrack() }
-
-    else if (e.code == "KeyC") { toggle_hidden('covers') }
-    else if (e.code == "KeyD") { toggle_hidden("metadata") }
-    else if (e.code == "KeyG") { toggle_hidden("tags") }
-    else if (e.code == "KeyH") { toggle_hidden("help") }
-    else if (e.code == "KeyI") { toggle_hidden('index') }
-    else if (e.code == "KeyL") { toggle_hidden("classical") }
-    else if (e.code == "KeyM") { toggle_hidden("related") }
-
     else if (e.code == "Digit1") { gotoTrack(1) }
     else if (e.code == "Digit2") { gotoTrack(2) }
     else if (e.code == "Digit3") { gotoTrack(3) }
