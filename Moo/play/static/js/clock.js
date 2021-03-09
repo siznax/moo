bda3d95da519fa65f4989b5294df9880fba73340
siznax <steve@siznax.net>
2021-03-09 13:33:44 -0800

    Fetch weather in clock.js

--- Moo/play/static/js/clock.js
+++ Moo/play/static/js/clock.js
@@ -1,12 +1,33 @@
-var time_elm = document.getElementById('time')
-
-function updateTime() {
-    if (time_elm) {
-        let time = new Date().toLocaleTimeString()
-        let ampm = time.split(" ")[1]
-        let hm_ = time.split(":", 2).join(":")
-        time_elm.innerHTML = hm_ + " " + ampm
+function fetchWeather() {
+    if (window.fetch) {
+        fetch("https://wttr.in/?format=%c")
+            .then(response => response.text())
+            .then(data => updateWeather(data))
     }
 }
 
+
+function updateTime() {
+    let elm = document.getElementById('time')
+
+    if (elm) {
+        let time = new Date().toLocaleTimeString()
+        let ampm = time.split(" ")[1]
+        let hm_ = time.split(":", 2).join(":")
+        elm.innerHTML = hm_ + " " + ampm
+    }
+}
+
+
+function updateWeather(conditions) {
+    let elm = document.getElementById('weather')
+
+    if (elm) {
+        elm.innerHTML = '<a href="https://wttr.in/" target="_blank">'
+            + conditions + "</a>"
+    }
+}
+
+
+fetchWeather()
 setInterval(updateTime, 1000)
