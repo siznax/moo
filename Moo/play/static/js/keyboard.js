b922cc6876b346121456fe86548eaa9daebd3590
siznax <steve@siznax.net>
2021-06-22 16:02:39 -0700

    Restored keyboard-specific fns from audio.js to keyboard.js

--- Moo/play/static/js/keyboard.js
+++ Moo/play/static/js/keyboard.js
@@ -1,32 +1,58 @@
-function keyDown(e) {
+var audio = document.querySelector("audio")
+var control = document.getElementById('control')
 
-    // alert(e.keyCode)
 
-    if (e.code == "ArrowUp") {
-        e.preventDefault()
-        gotoPrev() 
-    }
-    if (e.code == "ArrowRight") {
-        e.preventDefault()
-        gotoNext() 
-    }
-    if (e.code == "ArrowDown") {
-        e.preventDefault()
-        gotoNext() 
-    }
-    if (e.code == "ArrowLeft") {
-        e.preventDefault()
-        gotoPrev() 
+function gotoNext() {
+    let next = control.getAttribute('next')
+    if (next > 0) {
+        gotoTrack(next)
     }
 }
 
 
+function gotoPrev() {
+    let prev = control.getAttribute('prev')
+    if (prev > 0) {
+        gotoTrack(prev)
+    }
+}
+
+
+function gotoRandom() {
+    location = '/random'
+}
+
+
+function gotoRandomAlbum() {
+    let ralbum = control.getAttribute("ralbum")
+    let slash = ""
+    if (ralbum[0] != "/") {
+        slash = "/"
+    }
+    location = "/album" + slash + ralbum
+}
+
+
+function gotoTrack(num) {
+    let ntracks = parseInt(control.getAttribute('ntracks'))
+    if (parseInt(num) <= ntracks) {
+        let alkey = control.getAttribute("alkey")
+        location = "/track/" + num + "/" + alkey  // + "#" + num
+    }
+}
+
+
+function keyDown(e) {
+    if (e.code == "ArrowUp") { e.preventDefault(); gotoPrev() }
+    if (e.code == "ArrowRight") { e.preventDefault(); gotoNext() }
+    if (e.code == "ArrowDown") { e.preventDefault(); gotoNext() }
+    if (e.code == "ArrowLeft") { e.preventDefault(); gotoPrev() }
+}
+
+
 function keyPressed(e) {
-
-    // alert(e.code)
-
+    console.log('keyPressed: ' + e.code)
     if (e.code == "KeyN") { gotoNext() }
-
     else if (e.code == "KeyP") { gotoPrev() }
     else if (e.code == "KeyR") { gotoRandom() }
     else if (e.code == "KeyT") { playRandomTrack() }
@@ -39,10 +65,30 @@ function keyPressed(e) {
     else if (e.code == "Digit7") { gotoTrack(7) }
     else if (e.code == "Digit8") { gotoTrack(8) }
     else if (e.code == "Digit9") { gotoTrack(9) }
+    else if (e.code == "Digit0") { gotoTrack(10) }
+    else if (e.code == "Space") { e.preventDefault(); playOrPause() }
+    else {}
+}
 
-    else {
-        // ignore
+
+function playOrPause() {
+    if (audio) {
+        if (audio.paused) {
+            audio.play()
+        } else {
+            audio.pause()
+        }
     }
+}
+
+
+function playRandomTrack() {
+    gotoTrack(control.getAttribute("rtrack"))
+}
+
+
+if (audio) {
+    audio.addEventListener("ended", gotoNext)
 }
 
 
